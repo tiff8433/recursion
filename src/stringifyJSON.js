decb22dc547f68d924e0f6270840092befc7d6d7
tiff8433 <tiffany.l.huang@gmail.com>
2015-11-16 15:24:35 -0800

    Stringify arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+var newObj;
+  // check to see if object is a number
+  if(typeof obj === 'number'){
+    //return string of number
+    return '' + obj;
+  }
+  if(obj === null){
+    return '' + null;
+  }
+  if(typeof obj === 'boolean'){
+    return '' + obj;
+  }
+  if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  if(Array.isArray(obj)){
+    if(obj.length === 0){
+      return '[]';
+    }
+    newObj = 
+    for(var i = 1; i < obj.length; i++){
+
+    }
+
+  }
 };
