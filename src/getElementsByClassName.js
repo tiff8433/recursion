ec005c1e06370115aa8da9939ee9845d6ce4538e
tiff8433 <tiffany.l.huang@gmail.com>
2015-11-17 10:39:43 -0800

    getElementsByClassName passes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  var results = [];
+  if (document.body.classList.contains(className)) {
+      results.push(document.body);
+    }
+  function getClassName(node) {
+    
+    for (var i = 0; i < node.childNodes.length; i++) {
+      if (node.childNodes[i].classList !== undefined) {
+        if (node.childNodes[i].classList.contains(className)) {
+          results.push(node.childNodes[i]);
+        }
+        if (node.childNodes[i].childNodes.length > 0) {
+          getClassName(node.childNodes[i]);
+        }
+      }
+    }
+  }
+  getClassName(document.body);
+  return results;
 };
